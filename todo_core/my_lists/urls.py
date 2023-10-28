dc7f27cb1575f7815920b48bd4f0acc480af27ee
EimantasBlazevicius <eima.blaz@gmail.com>
2023-10-28 15:01:41 +0300

    templates, post, get

--- todo_core/my_lists/urls.py
+++ todo_core/my_lists/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import home
+from .views import home, items_list
 
 urlpatterns = [
-    path("", home)
+    path("", home),
+    path("<pk>/", items_list, name="items_list")
 ]
